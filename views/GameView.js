a8ceadb2522f256aaf3869ebaac71f89a97499e9
Maxime Coignus <maxime@DFE-MAC-00040.local>
2018-11-05 17:41:37 +0000

    Turn into a list

--- views/GameView.js
+++ views/GameView.js
@@ -1,9 +1,14 @@
 define(["marionette",
-        "text!../templates/game.tpl"], function(Marionette, gameTpl ) {
-    var GameView = Marionette.ItemView.extend({
+        "text!../templates/game.tpl",
+        "text!../templates/list_game.tpl"], function(Marionette, gameTpl, listGame ) {
+    var CollectionView = Marionette.CollectionView.extend({
+        template : _.template(listGame),
+        childViewContainer: 'js-game-list'
+    });
+    var TeamView = Marionette.ItemView.extend({
         template : _.template(gameTpl)
     });
 
-    return GameView;
+    return TeamView;
 
 });
